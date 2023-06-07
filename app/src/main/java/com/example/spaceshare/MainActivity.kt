57241acd3ce29ee6da4c7cd5ec6f324408b53ea2
jimmyfong0 <fzhiminn1957@gmail.com>
2023-06-07 12:04:19 -0400

    Setup project

---
+++ app/src/main/java/com/example/spaceshare/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.spaceshare
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+    }
+}
