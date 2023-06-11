37615e8fb5807da3e3a38e14798c2ed30593bea4
jimmyfong0 <fzhiminn1957@gmail.com>
2023-06-11 18:37:56 -0400

    Added navigation between core pages

--- app/src/main/java/com/example/spaceshare/MainActivity.kt
+++ app/src/main/java/com/example/spaceshare/MainActivity.kt
@@ -1,20 +1,53 @@
 package com.example.spaceshare
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.spaceshare.ui.view.ProfileFragment
+import com.example.spaceshare.ui.view.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
     companion object {
         private val TAG = this::class.simpleName
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // TODO: Set content view to search screen
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "Current user: %s".format(FirebaseAuth.getInstance().currentUser?.email))
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Set up the BottomNavigationView
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_search -> {
+                    navigateToFragment(SearchFragment())
+                    true
+                }
+                R.id.action_profile -> {
+                    navigateToFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun navigateToFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_nav_host_fragment, fragment)
+            .commit()
     }
 }
--- app/src/main/java/com/example/spaceshare/ui/view/ProfileFragment.kt
+++ app/src/main/java/com/example/spaceshare/ui/view/ProfileFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.spaceshare.R
 
-class SearchFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
