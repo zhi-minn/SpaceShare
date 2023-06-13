f9366616ce7f976c3e8a394864b37a7cc1c1a43d
Chang-Syuan Wu <william.wu1219@gmail.com>
2023-06-13 15:31:40 -0400

    basic functionality to publish a listing

--- app/src/main/java/com/example/spaceshare/models/Listing.kt
+++ app/src/main/java/com/example/spaceshare/models/Listing.kt
@@ -3,31 +3,19 @@ package com.example.spaceshare.models
 import java.util.Date
 
 data class Listing(
-    val id: String,
+    val id: String? = null,
+    val hostID: String? = null,
+    val photos: List<String>? = null,
     val title: String? = null,
+    val price: Double = 0.00,
     val description: String? = null,
     val location: String = "",
-    val price: Double = 0.00,
     // val availability: Availability? = null,
     // val size: String? = null,
-    val photos: List<String>? = null,
+
     // val amenities: List<String>? = null,
-    // val host: Host? = null,
+
     // val ratings: List<Rating>? = null
 ) {
-        data class Availability(
-            val startDate: Date? = null,
-            val endDate: Date? = null
-        )
 
-        data class Host(
-            val name: String? = null,
-            val contactInfo: String? = null,
-            val profilePicture: String? = null
-        )
-
-        data class Rating(
-            val rating: Double? = null,
-            val review: String? = null
-        )
 }
