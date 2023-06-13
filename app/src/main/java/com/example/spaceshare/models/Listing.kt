835e1af4fa5b814380feccf1b9f9d152c6a79904
jimmyfong0 <fzhiminn1957@gmail.com>
2023-06-13 14:51:39 -0400

    Setup Listing data and repositories

--- app/src/main/java/com/example/spaceshare/models/Listing.kt
+++ app/src/main/java/com/example/spaceshare/models/Listing.kt
@@ -1,5 +1,33 @@
 package com.example.spaceshare.models
 
-class Listing {
-    // TODO: Add relevant listing details
+import java.util.Date
+
+data class Listing(
+    val id: String,
+    val title: String? = null,
+    val description: String? = null,
+    val location: String = "",
+    val price: Double = 0.00,
+    // val availability: Availability? = null,
+    // val size: String? = null,
+    val photos: List<String>? = null,
+    // val amenities: List<String>? = null,
+    // val host: Host? = null,
+    // val ratings: List<Rating>? = null
+) {
+        data class Availability(
+            val startDate: Date? = null,
+            val endDate: Date? = null
+        )
+
+        data class Host(
+            val name: String? = null,
+            val contactInfo: String? = null,
+            val profilePicture: String? = null
+        )
+
+        data class Rating(
+            val rating: Double? = null,
+            val review: String? = null
+        )
 }
