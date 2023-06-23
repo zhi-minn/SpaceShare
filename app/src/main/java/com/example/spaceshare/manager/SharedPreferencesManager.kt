db1841cbc17fa4e8891cc1474155955c74230d6a
jimmyfong0 <fzhiminn1957@gmail.com>
2023-06-23 16:11:35 -0400

    SharedPreferences key unique per user

--- app/src/main/java/com/example/spaceshare/manager/SharedPreferencesManager.kt
+++ app/src/main/java/com/example/spaceshare/manager/SharedPreferencesManager.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.MutableLiveData
 
 object SharedPreferencesManager {
     private const val PREF_NAME = "SpaceSharePreferences"
-    const val HOST_MODE_KEY = "is_host_mode"
+    private lateinit var HOST_MODE_KEY: String
 
     private lateinit var sharedPreferences: SharedPreferences
     private val isHostModeLiveData = MutableLiveData<Boolean>()
@@ -15,8 +15,9 @@ object SharedPreferencesManager {
     val isHostMode: LiveData<Boolean>
         get() = isHostModeLiveData
 
-    fun init(context: Context) {
+    fun init(context: Context, uid: String?) {
         sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        HOST_MODE_KEY = "${uid}_is_host_mode"
         isHostModeLiveData.value = isHostMode()
     }
 
