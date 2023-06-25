ce1ce3ca7bc6be95181c45472a4af41b1c43c0a5
Chang-Syuan Wu <william.wu1219@gmail.com>
2023-06-25 19:38:59 -0400

    add date range selector

--- app/src/main/java/com/example/spaceshare/ui/view/SearchFragment.kt
+++ app/src/main/java/com/example/spaceshare/ui/view/SearchFragment.kt
@@ -6,22 +6,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.spaceshare.R
+import com.example.spaceshare.databinding.FragmentSearchBinding
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
 
 class SearchFragment : Fragment() {
 
+    private lateinit var binding: FragmentSearchBinding
+    private lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navController = requireActivity().findNavController(R.id.main_nav_host_fragment)
 
-        val whereCard = findViewById<CardView>(R.id.whereCard)
+        configureCards()
+    }
 
+    private fun configureCards() {
+        binding.whereCard.setOnClickListener {
+
+        }
+
+        // When
+        val constraintsBuilder = CalendarConstraints.Builder()
+            .setValidator(DateValidatorPointForward.now())
+        val dateRangePicker =
+            MaterialDatePicker.Builder.dateRangePicker()
+                .setTitleText("Select Dates")
+                .setCalendarConstraints(constraintsBuilder.build())
+                .build()
+        dateRangePicker.addOnPositiveButtonClickListener {
+            binding.searchTime.text = dateRangePicker.headerText
+        }
+        binding.whenCard.setOnClickListener {
+            dateRangePicker.show(parentFragmentManager, "tag")
+        }
     }
 
 }
