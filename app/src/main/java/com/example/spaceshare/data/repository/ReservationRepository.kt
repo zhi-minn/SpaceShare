4c30f5e337a0f55a84b901e4607df3ed86dc6a78
calvinsui <calvinsui@hotmail.com>
2023-06-25 20:43:53 -0400

    Reservation page view cards

--- app/src/main/java/com/example/spaceshare/data/repository/ReservationRepository.kt
+++ app/src/main/java/com/example/spaceshare/data/repository/ReservationRepository.kt
@@ -1,20 +1,15 @@
 package com.example.spaceshare.data.repository
 
-import com.example.spaceshare.data.implementation.ReservationRepoImpl
+import com.example.spaceshare.models.Listing
 import com.example.spaceshare.models.Reservation
 import com.example.spaceshare.models.User
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import dagger.Binds
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.FragmentComponent
+import com.google.android.gms.tasks.Task
 
 interface ReservationRepository {
 
-    fun createReservation(Reservation: Reservation) : String
+    suspend fun createReservation(Reservation: Reservation) : String
 
     suspend fun fetchReservations(user: User, asHost: Boolean): List<Reservation>
+
+//    suspend fun fetchListings(reservations: List<Reservation>?): List<Listing>
 }
