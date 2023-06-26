d3a03fc51ec66496a4b61720f54635d933ad9250
Chang-Syuan Wu <william.wu1219@gmail.com>
2023-06-25 20:43:16 -0400

    move search results fragment

--- app/src/main/java/com/example/spaceshare/ui/view/SearchResultsFragment.kt
+++ app/src/main/java/com/example/spaceshare/ui/view/SearchResultsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.spaceshare
+package com.example.spaceshare.ui.view
 
 import androidx.fragment.app.Fragment
 
@@ -6,9 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.spaceshare.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
