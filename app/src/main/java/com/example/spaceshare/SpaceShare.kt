b5592eca5cde33c13e77273f7a1d93dbec954b0d
jimmyfong0 <fzhiminn1957@gmail.com>
2023-06-27 00:12:51 -0400

    Improved UX for clicking images to full screen

--- app/src/main/java/com/example/spaceshare/SpaceShare.kt
+++ app/src/main/java/com/example/spaceshare/SpaceShare.kt
@@ -1,8 +1,18 @@
 package com.example.spaceshare
 
 import android.app.Application
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideOption
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class SpaceShare : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Initialize FirebaseApp
+        FirebaseApp.initializeApp(this)
+    }
 }
