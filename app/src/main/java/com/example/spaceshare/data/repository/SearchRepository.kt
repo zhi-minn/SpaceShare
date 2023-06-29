ce1be713531d59db72e452fea2207fc3abfa57de
jimmyfong0 <fzhiminn1957@gmail.com>
2023-06-28 23:59:08 -0400

    Deleted unnecessary repository

--- app/src/main/java/com/example/spaceshare/data/repository/SearchRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.spaceshare.data.repository
-
-import com.example.spaceshare.models.SearchCriteria
-import com.example.spaceshare.models.User
-
-interface SearchRepository {
-
-    suspend fun search(criteria : SearchCriteria) : List<SearchCriteria>
-}
