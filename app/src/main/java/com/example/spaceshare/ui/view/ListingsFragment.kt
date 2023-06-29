9cd34881c33d91c00b72ff02d73efd8fbb209d63
jimmyfong0 <fzhiminn1957@gmail.com>
2023-06-29 18:53:12 -0400

    Added search bar for host listings

--- app/src/main/java/com/example/spaceshare/ui/view/ListingsFragment.kt
+++ app/src/main/java/com/example/spaceshare/ui/view/ListingsFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
