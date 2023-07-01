6b81137bd7bbb3ecded0e231e71b788356fa773d
Chang-Syuan Wu <william.wu1219@gmail.com>
2023-07-01 18:42:43 -0400

    refactor google sso credentials processing to AuthViewModel

--- app/src/main/java/com/example/spaceshare/ui/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/spaceshare/ui/viewmodel/AuthViewModel.kt
@@ -1,18 +1,21 @@
 package com.example.spaceshare.ui.viewmodel
 
+import android.content.Intent
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.firebase.auth.AuthCredential
 import androidx.lifecycle.viewModelScope
 import com.example.spaceshare.data.repository.UserRepository
 import com.example.spaceshare.models.User
-import com.google.android.gms.auth.api.identity.SignInCredential
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.nulabinc.zxcvbn.Zxcvbn
 import kotlinx.coroutines.launch
@@ -22,7 +25,7 @@ import javax.inject.Inject
 class AuthViewModel @Inject constructor(
     private val auth: FirebaseAuth,
     private val userRepo: UserRepository
-): ViewModel() {
+) : ViewModel() {
 
     companion object {
         private val TAG = this::class.simpleName
@@ -32,6 +35,7 @@ class AuthViewModel @Inject constructor(
         val isSuccess: Boolean,
         val message: String
     )
+
     private val _loginStatus = MutableLiveData<AuthResult>()
     val loginStatus: LiveData<AuthResult> = _loginStatus
 
@@ -59,25 +63,84 @@ class AuthViewModel @Inject constructor(
                         _loginStatus.value = AuthResult(false, "Please verify your email first.")
                     }
                 } else {
-                    _loginStatus.value = AuthResult(false, "Authentication failed. ${task.exception?.message}")
+                    _loginStatus.value =
+                        AuthResult(false, "Authentication failed. ${task.exception?.message}")
                 }
             }
     }
 
-    fun loginWithSSOCredential(firebaseCredential: AuthCredential, signInCredential: SignInCredential) {
+    fun processGoogleSSOCredential(data: Intent?) {
+        try {
+            val credential = oneTapClient.getSignInCredentialFromIntent(data)
+            val idToken = credential.googleIdToken
+            when {
+                idToken != null -> {
+                    // Got an ID token from Google. Use it to authenticate with Firebase.
+                    val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+                    Log.d(TAG, "Got ID token.")
+
+                    var firstName = credential.givenName
+                    var lastName = credential.familyName
+
+                    if (firstName == null)
+                        firstName = ""
+                    if (lastName == null)
+                        lastName = ""
+
+                    loginWithSSOCredential(firebaseCredential, firstName, lastName)
+                }
+
+                else -> {
+                    // Shouldn't happen.
+                    Log.d(TAG, "No ID token!")
+                }
+            }
+        } catch (e: ApiException) {
+            when (e.statusCode) {
+                CommonStatusCodes.CANCELED -> {
+                    Log.d(TAG, "One-tap dialog was closed.")
+                    // Don't re-prompt the user.
+                    showOneTapUI = false
+                }
+
+                CommonStatusCodes.NETWORK_ERROR -> {
+                    Log.d(TAG, "One-tap encountered a network error.")
+                    // Try again or just ignore.
+                }
+
+                else -> {
+                    Log.d(
+                        TAG, "Couldn't get credential from result." +
+                                " (${e.localizedMessage})"
+                    )
+                }
+            }
+        }
+    }
+
+    private fun loginWithSSOCredential(
+        firebaseCredential: AuthCredential,
+        firstName: String,
+        lastName: String
+    ) {
         auth.signInWithCredential(firebaseCredential)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update with the signed-in user's information
                     Log.d(TAG, "signInWithCredential:success")
+                    _loginStatus.value = AuthResult(true, "Successfully logged in.")
 
                     // Create user in collection for custom fields
                     val user = auth.currentUser
-                    createUserInCollection(user?.uid, signInCredential.givenName, signInCredential.familyName)
-                    _loginStatus.value = AuthResult(true, "Successfully logged in.")
+                    createUserInCollection(
+                        user?.uid,
+                        firstName,
+                        lastName
+                    )
                 } else {
                     // Sign in failed.
-                    _loginStatus.value = AuthResult(false, "Authentication failed. ${task.exception?.message}")
+                    _loginStatus.value =
+                        AuthResult(false, "Authentication failed. ${task.exception?.message}")
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
                 }
             }
@@ -88,7 +151,7 @@ class AuthViewModel @Inject constructor(
             _registerStatus.value = AuthResult(false, "Please enter both email and password.")
             return
         }
-        
+
         val passwordScore = Zxcvbn().measure(password).score
         if (passwordScore == 0) {
             _registerStatus.value = AuthResult(false, "Please use a more secure password.")
@@ -110,18 +173,22 @@ class AuthViewModel @Inject constructor(
                             if (updateTask.isSuccessful) {
                                 _registerStatus.value = AuthResult(true, "Verification email sent")
                             } else {
-                                _registerStatus.value = AuthResult(false, "Registration successful but failed to update display name.")
+                                _registerStatus.value = AuthResult(
+                                    false,
+                                    "Registration successful but failed to update display name."
+                                )
                             }
                         }
                 } else {
-                    _registerStatus.value = AuthResult(false, "Registration failed. ${task.exception?.message}")
+                    _registerStatus.value =
+                        AuthResult(false, "Registration failed. ${task.exception?.message}")
                 }
             }
     }
 
     private fun createUserInCollection(userId: String?, firstName: String?, lastName: String?) {
-        if (userId != null) {
-            val user = User(userId, firstName ?: "", lastName ?: "")
+        if (userId != null && firstName != null && lastName != null) {
+            val user = User(userId, firstName, lastName)
             viewModelScope.launch {
                 try {
                     userRepo.createUser(user)
